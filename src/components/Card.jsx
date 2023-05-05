4782a1d4c088faf6ff68d545060cdbb5982268f5
ialcaine <101673351+irenealcaine@users.noreply.github.com>
2023-05-05 11:09:03 +0000

    Update Card.jsx and PlantsData.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,34 +1,54 @@
-import React from 'react'
-import { PlantsData } from '../data/PlantsData'
-import water from '../assets/images/drop.svg'
-import light from '../assets/images/sun.svg'
+import React from "react";
+import { PlantsData } from "../data/PlantsData";
+import water from "../assets/images/drop.svg";
+import light from "../assets/images/sun.svg";
 
 const Card = () => {
   return (
     <>
-      <div className='bg-green-50 bg-opacity-60 shadow-2xl flex md:flex-row flex-col items-center justify-center md:items-start md:px-4 md:py-2 p-0.5 flex-wrap gap-5 rounded-lg'>
-
-        {
-          PlantsData.map((data, id) => {
-            return (
-
-              <div key={id} className="border border-green-400 shadow-xl rounded-lg md:w-80 w-full bg-white">
-                <p className='text-3xl text-bold text-center py-4 font-caveat'>{data.name}</p>
-                <img src={data.img} alt="" className='w-full' />
-                <div className='flex justify-center mt-4'>
-                  <div className='flex items-center px-4'><img src={water} alt="" className='w-6' /> - <img src={data.water} alt="" className='w-6' /> </div>
-                  <div className='flex items-center px-4'><img src={light} alt="" className='w-6' /> - <img src={data.light} alt="" className='w-6' /></div>
+      <div className="bg-green-50 bg-opacity-60 shadow-2xl flex md:flex-row flex-col items-center justify-center md:items-start md:px-4 md:py-2 p-0.5 flex-wrap gap-5 rounded-lg">
+        {PlantsData.map((data, id) => {
+          return (
+            <div
+              key={id}
+              className="border border-green-400 shadow-xl rounded-lg md:w-80 w-full bg-white"
+            >
+              <p className="text-3xl text-bold text-center py-4 font-caveat">
+                {data.name}
+              </p>
+              <img src={data.image} alt="" className="w-full" />
+              <div className="flex justify-center mt-4">
+                <div className="flex items-center px-4">
+                  <img src={water} alt="" className="w-6" /> -{" "}
+                  <img src={data.water} alt="" className="w-6" />{" "}
                 </div>
-                <p className='indent-4 text-justify px-4 mt-4 text-sm md:text-base'>{data.description}</p>
-                <div className={` text-center py-1 border-2 rounded-lg  m-0.5 mt-4  ${data.category === 'Flor' ? 'border-rose-600 bg-rose-100' : data.category === 'Decorativa' ? 'border-fuchsia-600 bg-fuchsia-100' : data.category === 'Culinaria' ? 'border-lime-600 bg-lime-100' : 'border-orange-600 bg-orange-100'}`}>{data.category}</div>
+                <div className="flex items-center px-4">
+                  <img src={light} alt="" className="w-6" /> -{" "}
+                  <img src={data.light} alt="" className="w-6" />
+                </div>
               </div>
-            )
-          })
-        }
-
+              <p className="indent-4 text-justify px-4 mt-4 text-sm md:text-base">
+                {data.description}
+              </p>
+              <div
+                className={` text-center py-1 border-2 rounded-lg  m-0.5 mt-4  ${
+                  data.category === "Flor"
+                    ? "border-rose-600 bg-rose-100"
+                    : data.category === "Decorativa"
+                    ? "border-fuchsia-600 bg-fuchsia-100"
+                    : data.category === "Culinaria"
+                    ? "border-lime-600 bg-lime-100"
+                    : "border-orange-600 bg-orange-100"
+                }`}
+              >
+                {data.category}
+              </div>
+            </div>
+          );
+        })}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
--- src/data/PlantsData.jsx
+++ src/data/PlantsData.jsx
@@ -1,47 +1,111 @@
-import low from '../assets/images/low.svg'
-import medium from '../assets/images/medium.svg'
-import high from '../assets/images/high.svg'
+import low from "../assets/images/low.svg";
+import medium from "../assets/images/medium.svg";
+import high from "../assets/images/high.svg";
 
 export const PlantsData = [
   {
     id: 1,
-    name: 'Rosa',
-    img: 'src/assets/images/rosal.jpg',
-    water: low,
-    light: high,
-    description: 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.',
-    category: 'Flor'
-  }, {
-    id: 2,
-    name: 'Aloe vera',
-    img: 'src/assets/images/aloe-vera.jpg',
-    water: medium,
-    light: high,
-    description: 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.',
-    category: 'Decorativa'
-  }, {
-    id: 3,
-    name: 'Planta',
-    img: 'src/assets/images/seto.jpeg',
+    name: "Orquídea",
+    description: "Una planta ornamental muy valorada por sus flores exóticas",
+    category: "Flor",
+    image:
+      "https://media.admagazine.com/photos/618a616c8124d089603b1cb7/master/w_1600%2Cc_limit/85878.jpg",
     water: low,
     light: medium,
-    description: 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.',
-    category: 'Culinaria'
-  }, {
-    id: 4,
-    name: 'Hierbabuena',
-    img: 'src/assets/images/menta.jpg',
-    water: medium,
-    light: high,
-    description: 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.',
-    category: 'Interior'
-  }, {
-    id: 5,
-    name: 'Rosa',
-    img: 'src/assets/images/rosal.jpg',
+  },
+  {
+    id: 2,
+    name: "Helecho",
+    description: "Una planta que crece en lugares húmedos y sombríos",
+    category: "Interior",
+    image:
+      "https://cdn.pixabay.com/photo/2019/05/06/19/13/green-4183977_1280.jpg",
+    water: high,
+    light: low,
+  },
+  {
+    id: 3,
+    name: "Cactus",
+    description:
+      "Una planta que se adapta bien a climas áridos y con poca agua",
+    category: "Decorativa",
+    image:
+      "https://images.hola.com/imagenes/decoracion/20230120224568/cactus-erizo-cultivo-cuidados-plantas-exterior-nu/1-189-747/cactus-erizo-cultivo-cuidados-hola-decoracion-m.jpg?tx=w_680",
     water: low,
     light: high,
-    description: 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.',
-    category: 'Decorativa'
   },
-]
+  {
+    id: 4,
+    name: "Lavanda",
+    description:
+      "Una planta aromática muy valorada por su perfume y propiedades medicinales",
+    category: "Culinaria",
+    image:
+      "https://www.hogarmania.com/archivos/202202/lavanda-planta-medicinal-para-tratar-heridas-y-quemaduras-1-1280x720x80xX.jpg",
+    water: medium,
+    light: high,
+  },
+  {
+    id: 5,
+    name: "Rosa",
+    description: "Una planta ornamental muy valorada por sus flores de colores",
+    category: "Flor",
+    image:
+      "https://www.jardineriaon.com/wp-content/uploads/2015/10/rosa-te.jpg",
+    water: medium,
+    light: high,
+  },
+  {
+    id: 6,
+    name: "Tomillo",
+    description:
+      "Una planta aromática muy valorada por su sabor y propiedades medicinales",
+    category: "Culinaria",
+    image:
+      "https://www.dosfarma.com/blog/wp-content/uploads/2018/09/15-09-2018-propiedades-tomillo.jpg",
+    water: low,
+    light: high,
+  },
+  {
+    id: 7,
+    name: "Geranio",
+    description:
+      "Una planta ornamental muy valorada por sus flores de colores y su resistencia",
+    category: "Flor",
+    image:
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/geranios-rosas-y-rojos-642d405da34f3.jpg?crop=1.00xw:0.676xh;0,0&resize=1200:*",
+    water: medium,
+    light: high,
+  },
+  {
+    id: 8,
+    name: "Hiedra",
+    description: "Una planta trepadora que crece bien en lugares sombríos",
+    category: "Interior",
+    image:
+      "https://media.floresfrescasonline.com/product/hedera-hibernica-hiedra-800x800.jpeg",
+    water: high,
+    light: low,
+  },
+  {
+    id: 9,
+    name: "Aloe Vera",
+    description:
+      "Una planta suculenta muy valorada por sus propiedades medicinales y cosméticas",
+    category: "Decorativa",
+    image:
+      "https://www.mentta.com/blog/wp-content/uploads/2021/02/aloe-vera.jpg",
+    water: low,
+    light: medium,
+  },
+  {
+    id: 10,
+    name: "Menta",
+    description:
+      "Una planta aromática muy valorada por su sabor y propiedades medicinales",
+    category: "Culinaria",
+    image: "https://mejorconsalud.as.com/wp-content/uploads/2013/08/menta.jpg",
+    water: medium,
+    light: medium,
+  },
+];
